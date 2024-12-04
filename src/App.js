b4a12366f04e8747dc91abc2234a43a11ea41476
Andrey Kristev <hhghv49@gmail.com>
2024-12-04 18:05:20 +0300

    init2

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "./components/Shared/Header";
+import Footer from "./components/Shared/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h2>About Us</h2>
+        <p>This is an example landing page created with React.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
