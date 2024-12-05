3bf56f816d4c64bb41954133168c6d547cde2ab1
Andrey Kristev <141245468+Felirian@users.noreply.github.com>
2024-12-05 14:43:15 +0300

    add: ngrok for tg (src) - ngrok for build

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Header from "./components/Shared/Header";
 import Footer from "./components/Shared/Footer";
 
 function App() {
+
+
+
   return (
     <div className="App">
       <Header />
       <main>
-        <h2>About Us</h2>
+        <h2>About Us ggg gfgf</h2>
         <p>This is an example landing page created with React.</p>
       </main>
       <Footer />
