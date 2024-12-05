62823d228be3ad73c1b4564c0c4edff975904ce5
Andrey Kristev <141245468+Felirian@users.noreply.github.com>
2024-12-05 17:42:34 +0300

    add: fullpage (src) - fullpage open on init proj

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from "./components/Shared/Header";
 import Footer from "./components/Shared/Footer";
 
 function App() {
+  const tgApp = Telegram.WebApp
+  console.log(tgApp);
 
-
-
+  const click = () => {
+    tgApp.requestFullscreen()
+  }
   return (
     <div className="App">
       <Header />
       <main>
         <h2>About Us ggg gfgf</h2>
+        <h2>Кто любит огурцы </h2>
+        <button onClick={click}>ggg</button>
         <p>This is an example landing page created with React.</p>
       </main>
       <Footer />
---
+++ src/features/telegram_init.ts
@@ -0,0 +1,78 @@
+import {init, miniApp, mockTelegramEnv, parseInitData, mainButton, shareURL} from "@telegram-apps/sdk";
+
+const initializeTelegramSDK = async () => {
+  try {
+    // Попытка инициализировать настоящее окружение Telegram
+    console.log("Инициализация окружения Telegram");
+    const [miniApp]: any = init();
+    await miniApp.ready();
+  } catch (error) {
+    // В случае ошибки инициализируем фейковое окружение
+    console.error('Ошибка при инициализации Telegram:', error);
+    alert(`Ошибка при инициализации Telegram: ${error.toString()}`);
+    const initDataRaw = new URLSearchParams([
+      ['user', JSON.stringify({
+        id: 99281932,
+        first_name: 'Andrew',
+        last_name: 'Rogue',
+        username: 'rogue',
+        language_code: 'en',
+        is_premium: true,
+        allows_write_to_pm: true,
+      })],
+      ['hash', '89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31'],
+      ['auth_date', '1716922846'],
+      ['start_param', 'debug'],
+      ['chat_type', 'sender'],
+      ['chat_instance', '8428209589180549439'],
+    ]).toString();
+
+    mockTelegramEnv({
+      themeParams: {
+        accentTextColor: '#6ab2f2',
+        bgColor: '#17212b',
+        buttonColor: '#5288c1',
+        buttonTextColor: '#ffffff',
+        destructiveTextColor: '#ec3942',
+        headerBgColor: '#fcb69f',
+        hintColor: '#708499',
+        linkColor: '#6ab3f3',
+        secondaryBgColor: '#232e3c',
+        sectionBgColor: '#17212b',
+        sectionHeaderTextColor: '#6ab3f3',
+        subtitleTextColor: '#708499',
+        textColor: '#f5f5f5',
+      },
+      initData: parseInitData(initDataRaw),
+      initDataRaw,
+      version: '8.0',
+      platform: 'tdesktop',
+    });
+
+    console.log('Mock Telegram environment initialized');
+  }
+
+  miniApp.mount()
+  miniApp.setHeaderColor('#883a3a');
+
+
+  mainButton.mount()
+  mainButton.setParams({
+    backgroundColor: '#e715a9',
+    text: 'Поделиться игрой',
+    isVisible: true,
+    isEnabled: true,
+  });
+
+  // Установка обработчика нажатия на главную кнопку
+  mainButton.onClick( () => {
+    try {
+      shareURL(`хей заходи играть в наш дуолинго чел`);
+      console.log('Окно выбора чата открыто для отправки сообщения.');
+    } catch (error) {
+      console.error('Ошибка при открытии окна выбора чата:', error);
+    }
+  });
+};
+
+export default initializeTelegramSDK;
