253dce956cc7699833e0697c3e1b6f437fc32871
Andrey Kristev <141245468+Felirian@users.noreply.github.com>
2024-12-16 06:12:18 +0300

    add: text tags (styles)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import {Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps} fro
 import Document from "next/document";
 import {ServerStyleSheet} from "styled-components";
 import Script from "next/script";
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
 
@@ -31,6 +32,11 @@ class MyDocument extends Document {
     return (
       <Html lang="ru">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+            rel="stylesheet"/>
           {/*<meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>*/}
           {/*<meta name="viewport" content="width=device-width, user-scalable=no"/>*/}
           {/*<meta name="mobile-web-app-capable" content="yes"/>*/}
--- src/styles/textTags.ts
+++ src/styles/textTags.ts
@@ -1,14 +1,70 @@
 import styled, { css } from 'styled-components';
 
-export const H1Style = css`
-  text-align: center;
-  font-family: 'Singel';
-  font-size: 8.97vw;
+export const H1Style = css` 
+  font-family: Montserrat;
+  font-size: 7.22vw;
   font-style: normal;
-  font-weight: 600;
-  line-height: 106%;
-  color: inherit;
+  font-weight: 700;
+  line-height: 135%; /* 35.1px */
 `;
+
+export const H2Style = css`
+ font-family: Montserrat;
+  font-size: 6.11vw;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 135%; /* 29.7px */
+`;
+
+export const H3Style = css`
+  font-family: Montserrat;
+  font-size: 5.28vw;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 125.011%; /* 23.752px */
+`;
+
+export const Btn1Style = css`
+  font-family: Nunito;
+  font-size: 6.67vw;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+`;
+
+export const Btn2Style = css`
+  font-family: Montserrat;
+  font-size: 5vw;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 135%; /* 24.3px */
+`;
+
+
+export const Btn3Style = css` 
+  font-family: Montserrat;
+  font-size: 3.61vw;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 135%; /* 17.55px */
+`;
+
+export const Body1Style = css`
+  font-family: "DM Sans";
+  font-size: 3.89vw;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 125%; /* 17.5px */
+`;
+
+export const Body2Style = css`
+  font-family: "DM Sans";
+  font-size: 4.44vw;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 125%; /* 20px */
+`;
+
 
 export const H1 = styled.h1`
   ${H1Style};
