07907149564a24cf7a00f8835dd9b0a91c9ba601
shonenenenene <danya.sytaev@gmail.com>
2024-12-18 16:22:25 +0300

    edit: Points -complete

--- src/components/Points/PointItem.tsx
+++ src/components/Points/PointItem.tsx
@@ -19,6 +19,8 @@ const PointItem = ({ data, coordinates, currentPoint }: PointItemProps) => {
   };
 
   const isLockedPoint = data.number > currentPoint;
+  console.log(data, 'itemdata');
+  console.log(isLockedPoint);
 
   return (
     <PointItemWr
@@ -29,7 +31,7 @@ const PointItem = ({ data, coordinates, currentPoint }: PointItemProps) => {
       data-aos-duration='700'
       disabled={isLockedPoint}
     >
-      <PointBtn1>
+      <PointBtn1 $islocked={isLockedPoint}>
         <span>{isLockedPoint ? <PointsSvgSelector name='locked' /> : data.number}</span>
         <PointsSvgSelector name='point' />
 
@@ -59,19 +61,12 @@ const PointItemWr = styled.button`
       }
     }
   }
-
   &:disabled {
-    svg {
-      ellipse {
-        fill: ${COLORS.mediumOrange};
-        filter: drop-shadow(0px 1.7vw 0px ${COLORS.heavyOrange})
-          drop-shadow(0 0.8vw 0.8vw rgb(0, 0, 0, 0.25));
-      }
-    }
+    pointer-events: none;
   }
 `;
 
-const PointBtn1 = styled(Btn1)`
+const PointBtn1 = styled(Btn1)<{ $islocked: boolean }>`
   position: relative;
 
   width: 100%;
@@ -102,9 +97,16 @@ const PointBtn1 = styled(Btn1)`
     width: 99%;
     height: 99%;
     z-index: 0;
-
-    filter: drop-shadow(0px 1.7vw 0px ${COLORS.mediumOrange})
-      drop-shadow(0 0.8vw 0.8vw rgb(0, 0, 0, 0.25));
+    filter: ${({ $islocked }) =>
+      $islocked
+        ? `drop-shadow(0px 1.7vw 0px ${COLORS.darkOrange}) drop-shadow(0 0.8vw 0.8vw rgb(0, 0, 0, 0.25))`
+        : `drop-shadow(0px 1.7vw 0px ${COLORS.mediumOrange}) drop-shadow(0 0.8vw 0.8vw rgb(0, 0, 0, 0.25))`};
+    path {
+      fill: ${({ $islocked }) => ($islocked ? COLORS.lightOrange : COLORS.softOrange)};
+    }
+    ellipse {
+      fill: ${({ $islocked }) => ($islocked ? COLORS.mediumOrange : COLORS.orange)};
+    }
   }
 `;
 
