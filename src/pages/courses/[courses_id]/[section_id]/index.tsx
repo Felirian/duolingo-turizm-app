7c1413da54acdd3243f41ee9ff49d5cb24c306ee
Daniil Sytaev <danya.sytaev@gmail.com>
2025-08-07 17:39:13 +0300

    fix smth

--- src/pages/courses/[courses_id]/[section_id]/index.tsx
+++ src/pages/courses/[courses_id]/[section_id]/index.tsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Page } from '@/components/Shared/Page';
 import Points from '@/components/Points';
 import { useGetAllPoints } from '@/features/_queries_/_rest_api_';
 import BottomTabs from '@/components/Shared/BottomTabs';
 import Loader from '@/components/Shared/Loader';
-import useTgApp from "@/features/_tg_methods_";
+import useTgApp from '@/features/_tg_methods_';
 
 const Index = () => {
   const router = useRouter();
@@ -14,9 +14,10 @@ const Index = () => {
 
   const { loading, data, error } = useGetAllPoints(id, userId);
 
+  // console.log(data, 'useGetAllPoints');
 
   useEffect(() => {
-    setGg(!gg)
+    setGg(!gg);
   }, [router]);
 
   if (!router.isReady) {
